a1f3e79b5deaa8a1dd8ca4edecdfe9a84a10050c
brt <brt@hotmail.com>
2017-11-08 09:04:27 -0500

    - commit 2

---
+++ djfjsd/djfjsd/djfjsd.cpp
@@ -0,0 +1,16 @@
+// djfjsd.cpp: define el punto de entrada de la aplicación de consola.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	printf("\nPrueba 2");
+
+
+	printf("\nPrueba 2");
+	getchar();
+	return 0;
+}
+
